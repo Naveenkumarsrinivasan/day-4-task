da2ae833c7e4a7db7d05189bf23090be89257d49
Naveenkumarsrinivasan <naveenkumarece96@gmail.com>
2023-08-22 23:02:03 +0900

    first commit

---
+++ json.resume/script.js
@@ -0,0 +1,39 @@
+let myResume={
+    "basics": {
+      "name": "NAVEENKUMAR S",
+      "email": "naveenkumar@gmail.com",
+      "phone": 8012556699,
+      "degree": "B.E",
+      "location": {
+        "address": "308,anna street,andimadam",
+        "postalCode": 608901,
+        "city": "Ariyalur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "work": [
+        {
+          "company": "KIA MOTORS",
+          "position": "production team",
+          "startDate": "2019-09-25",
+          "summary": "i am the production team machine handling working in four years experience",
+        },
+      ],
+      "education": [
+        {
+          "institution": "muthayammal College Of Engineering",
+          "department": "Electronics Communication Engineering",
+          "studyType": "fulltime",
+          "batch start year": 2014,
+          "batch end year": 2017,
+          "gpa": 6.5,
+        }
+      ],
+      "languages": [
+        {
+          "language": "Telugh,Tamil,English,korean",
+        }
+      ],
+    }
+}
+console.log(myResume);
